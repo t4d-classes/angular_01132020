740e290d4430926978dee2e39d273f47ffc28cec
Eric Greene <eric@t4d.io>
2020-01-13 14:10:26 -0600

    add code demos

--- demo-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -32,7 +32,29 @@ export class ColorHomeComponent implements OnInit {
     this.colorForm = this.fb.group({
       name: '',
     });
+  }
 
+  doAddColor() {
+
+    const newColor = {
+      name: this.colorForm.value.name,
+      id: Math.max(...this.colors.map(color => color.id), 0) + 1,
+    };
+
+    // this.colors = this.colors.concat(newColor);
+
+    this.colors = [
+      { id: 1, name: 'blue' },
+      { id: 2, name: 'green' },
+      { id: 3, name: 'yellow' },
+      { id: 4, name: 'white' },
+      newColor,
+    ];
+
+  }
+
+  trackByIdName(color: { id: number, name: string }) {
+    return color.id + ':' + color.name;
   }
 
 }
